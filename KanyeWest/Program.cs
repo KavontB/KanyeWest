9c4fca5c3d956cb3e39a623a7dcf3b0faafe1d25
Kavont Brown <kavontb@gmail.com>
2020-11-23 19:38:30 -0600

    Add project files.

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            string chuck = "https://api.chucknorris.io/jokes/random";
+
+            var chuckResponse = client.GetStringAsync(chuck).Result;
+
+            Console.WriteLine(chuckResponse);
+
+            var chuckQuote = JObject.Parse(chuckResponse).GetValue("value").ToString();
+
+            Console.WriteLine(chuckQuote);
+        }
+    }
+}
