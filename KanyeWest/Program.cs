2b0de8746be1076d935a6dd9b95f172ae8b13fe6
Kavont Brown <kavontb@gmail.com>
2021-01-19 17:43:34 -0600

    kanyerest

--- KanyeWest/Program.cs
+++ KanyeWest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using Newtonsoft.Json.Linq;
+using RestSharp;
 
 namespace KanyeWest
 {
@@ -10,15 +11,37 @@ namespace KanyeWest
         {
             var client = new HttpClient();
 
-            string chuck = "https://api.chucknorris.io/jokes/random";
+            var kanyeURL = "https://api.kanye.rest";
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            int counter = 0;
+            while (counter < 6)
+            {
+                var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var ronResponse = client.GetStringAsync(ronURL).Result;
 
-            var chuckResponse = client.GetStringAsync(chuck).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            
+                counter++;
+                Console.WriteLine($"Ron: {ronQuote}");
+                Console.WriteLine("_-_-_-_-_-_-_-_-_-_-_-");
+                Console.WriteLine($"Kanye: {kanyeQuote}");
+                Console.WriteLine("_-_-_-_-_-_-_-_-_-_-_-");
+            }
+        }
+        public static IRestResponse CallOfDuty()
+        {
+            var client = new RestClient("https://call-of-duty-modern-warfare.p.rapidapi.com/multiplayer/Chob%252321309/battle");
 
-            Console.WriteLine(chuckResponse);
+            var request = new RestRequest(Method.GET);
 
-            var chuckQuote = JObject.Parse(chuckResponse).GetValue("value").ToString();
+            request.AddHeader("x-rapidapi-key", "SIGN-UP-FOR-KEY");
 
-            Console.WriteLine(chuckQuote);
+            request.AddHeader("x-rapidapi-host", "call-of-duty-modern-warfare.p.rapidapi.com");
+
+            IRestResponse response = client.Execute(request);
+
+            return response;
         }
     }
 }
